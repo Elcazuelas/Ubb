84fd92619ce244c9cca934898c7e4e02c5439bfd
El cazuelas <julio.insuco@gmail.com>
2020-11-17 17:04:37 -0300

    Le quité puto a mi nombre

--- src/presentacion/UIPrincipal.java
+++ src/presentacion/UIPrincipal.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  * 
- * @author Julio Monroy (puto)- Camilo Vazques
+ * @author Julio Monroy - Camilo Vazques
  */
 public class UIPrincipal {
     private static UIPrincipal instance;
