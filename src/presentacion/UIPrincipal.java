54faa04af9482f90690efed57f523cda691a694e
IgnacioVR <74623802+IgnacioVR@users.noreply.github.com>
2020-11-18 00:38:30 -0300

    Update UIPrincipal.java

--- src/presentacion/UIPrincipal.java
+++ src/presentacion/UIPrincipal.java
@@ -38,6 +38,62 @@ public class UIPrincipal {
     }
     
     private void menuMain(){
+        int opcion=0;
+        do{
+            System.out.println("MENU PRINCIPAL");
+            System.out.println("==============");
+            System.out.println("1.  Crear un cliente");
+            System.out.println("2.  Crear un veterinario");
+            System.out.println("3.  Crear una mascota");
+            System.out.println("4.  Listar mascota de una clase");
+            System.out.println("5.  Agregar una atencion a una mascota");
+            System.out.println("6.  Listar ultima atencion de mascotas de un cliente");
+            System.out.println("7.  Listar atenciones de un veterinario");
+            System.out.println("8.  Listar Nro.atenciones para clase, especie y raza en un lapso de tiempo");
+            System.out.println("9.  Listar datos y nro.atenciones de cada veterinario");
+            System.out.println("10. Salir");
+            System.out.print("        Ingrese onpcion:");
+            opcion=tdd.nextInt();
+            
+            switch (opcion){
+                case 1:
+                    creaCliente();
+                    break;
+                case 2:
+                    creaVeterinario();
+                    break;
+                case 3:
+                    creaMascota();
+                    break;
+                case 4:
+                    listasMascotasDeUnaClase();
+                    break;
+                case 5:
+                    //falta
+                    System.out.println("Estamos trabjando para esta opcion");
+                    break;
+                case 6:
+                    //falta
+                    System.out.println("Estamos trabjando para esta opcion");
+                    break;
+                case 7:
+                    //falta
+                    System.out.println("Estamos trabjando para esta opcion");
+                    break;
+                case 8:
+                    //falta
+                    System.out.println("Estamos trabjando para esta opcion");
+                    break;
+                case 9:
+                    listaDatosVeterinario();
+                    break;
+                case 10:
+                    //agrego opcion 10 para ñadir un default
+                    System.out.println("Adios :(");
+                default:
+                    System.out.println("Elija una opcion valida");
+            }
+        }while(opcion!=10);
     }
     
 }
